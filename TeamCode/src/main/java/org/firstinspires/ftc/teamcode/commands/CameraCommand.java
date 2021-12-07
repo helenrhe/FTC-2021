4fceb166d3bb92641a1622c7eba3227dc5e58de8
Avery Keuben <avery1516@gmail.com>
2021-12-07 16:32:48 -0700

    WIP: Camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CameraCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CameraCommand.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode.commands;
 
-import android.hardware.camera2.CameraCaptureSession;
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.hardware.camera2.CaptureRequest;
+import android.os.Handler;
+import androidx.annotation.NonNull;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.RobotLog;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraManager;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.android.util.Size;
+import org.firstinspires.ftc.robotcore.external.function.Continuation;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.*;
+import org.firstinspires.ftc.robotcore.internal.network.CallbackLooper;
 import org.firstinspires.ftc.robotcore.internal.system.Deadline;
 import org.firstinspires.ftc.structure.SubCommand;
 
@@ -19,8 +24,12 @@ public class CameraCommand implements SubCommand {
     private Camera camera;
     private CameraCaptureSession cameraSession;
 
+    private Handler callbackHandler;
+
     @Override
     public void onInit(OpMode opMode) {
+        callbackHandler = CallbackLooper.getDefault().getHandler();
+
         manager = ClassFactory.getInstance().getCameraManager();
         cameraName = opMode.hardwareMap.get(WebcamName.class, "Webcam 1");
 
@@ -29,6 +38,31 @@ public class CameraCommand implements SubCommand {
             throw new IllegalStateException("Camera permission not granted!");
         }
 
+        int format = ImageFormat.YUY2;
+
+        CameraCharacteristics cameraCharacteristics = cameraName.getCameraCharacteristics();
+
+        Size size = cameraCharacteristics.getDefaultSize(format);
+        int fps = cameraCharacteristics.getMaxFramesPerSecond(format, size);
+
+        try {
+            camera.createCaptureSession(Continuation.create(callbackHandler, new org.firstinspires.ftc.robotcore.external.hardware.camera.CameraCaptureSession.StateCallbackDefault() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    try {
+                        CameraCaptureRequest request = camera.createCaptureRequest(format, size, fps);
+                        session.startCapture(request, (CameraCaptureSession.CaptureCallback) (cameraCaptureSession, cameraCaptureRequest, cameraFrame) -> {
+                            Bitmap bmp = request.createEmptyBitmap();
+                            cameraFrame.copyToBitmap(bmp);
+                        }, Continuation.create(callbackHandler, (session1, cameraCaptureSequenceId, lastFrameNumber) -> {}));
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }));
+        } catch (CameraException e) {
+
+        }
     }
 
     @Override
